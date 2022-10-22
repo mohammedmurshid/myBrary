93701a6ae9a3566708d0327e68c9e4769532dbb2
Mohammed Murshid <murshid434@gmail.com>
2022-10-22 13:27:41 +0530

    initial Author routes

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
 
-const indexRouter = require("./routes/index");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}))
+
+// setting up routers
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors")
+
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 // setting up database
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,6 +32,5 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 5000);
